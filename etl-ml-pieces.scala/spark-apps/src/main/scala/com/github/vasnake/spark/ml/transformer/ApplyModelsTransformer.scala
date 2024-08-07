5bd4ff99d40965c789fab839e77a5cfab3e3ac52
Valentin Fedulov <vasnake@gmail.com>
2024-08-07 14:19:50 +0300

    minor fixes

--- etl-ml-pieces.scala/spark-apps/src/main/scala/com/github/vasnake/spark/ml/transformer/ApplyModelsTransformer.scala
+++ etl-ml-pieces.scala/spark-apps/src/main/scala/com/github/vasnake/spark/ml/transformer/ApplyModelsTransformer.scala
@@ -24,31 +24,36 @@ import com.github.vasnake.spark.io.HDFSFileToolbox
 import com.github.vasnake.json.read.{ModelConfig => ModelsConfigJson}
 
 /**
-  * APPLY stage task load batch of models from models descriptions and apply that batch
+  * APPLY stage: load batch of models from models descriptions and apply that batch
   * to each partition of input dataset (call model.transform(row) for each model and each row of the dataset.
-  * You can consider this as 'explode' behavior.
+  * You can consider this as an 'explode' behavior.
   *
-  * Complete config consists of some index in form of (key -> value) map,
+  * Complete config consists of some index (directory) in form of (key -> value) map,
   * json files and possibly pmml files.
   * All those files should be created by external process during LEARN stage.
   *
   * @param uid spark.ml pipeline stage id
   */
 class ApplyModelsTransformer(override val uid: String) extends Transformer with DefaultParamsWritable {
-  // TODO: convert 'transformer' to 'model', that model produced by 'estimator' 'fit' method.
-  // Only fit don't actually fit model to dataset but load model (batch of models) from storage.
+  // TODO: consider converting 'transformer' to 'model'; that model produced by 'estimator' 'fit' method.
+  // Only fit doesn't actually fit model to dataset but load model (batch of models) from storage.
   // In that case we could eliminate 'init' and lazy config loader in transformer code.
+  // Or, you can use 'load' interface to perform 'init' functionality.
 
   def this() = this(Identifiable.randomUID("apply_models"))
 
-  // transformer parameter: list of models encoded to string by custom encoder
+  /**
+   * transformer parameter: list of models encoded to string by custom encoder
+   */
   final val models = new Param[String](this, "models", "List of models descriptions")
   setDefault(models, "")
   def setModels(value: String): this.type = set(models, value)
   def getModels: String = getOrDefault(models)
 
-  // transformer parameter: df columns to keep
-  final val keep_columns = new Param[String](this, "keep_columns", "List of columns names to keep in output dataset")
+  /**
+   * transformer parameter: df columns to keep
+   */
+  final val keep_columns = new Param[String](this, "keep_columns", "List of columns names to keep during transformation")
   setDefault(keep_columns, "")
   def setKeepColumns(value: String): this.type  = set(keep_columns, value)
   def getKeepColumns: String = getOrDefault(keep_columns)
@@ -82,7 +87,7 @@ class ApplyModelsTransformer(override val uid: String) extends Transformer with 
     val keepColsNames = config.keepColumns
     val modelObjects = config.models.filter(_.isOK)
 
-    // all features that we need
+    // all features that we need (GF: Grouped Features)
     val mergedGFs = GroupedFeatures.mergeGroupedFeatures(
       modelObjects.map(_.groupedFeatures)
     )
@@ -98,17 +103,18 @@ class ApplyModelsTransformer(override val uid: String) extends Transformer with 
 
     logInfo("broadcasting data ...")
     val broadcasted = {
+      // array of indices for each model
       val featuresIndices = modelObjects.map(model =>
         GroupedFeatures.computeFeaturesIndices(mergedGFs, model.groupedFeatures)
       )
 
-      val tdc = ApplyModelsDistributedConfig(
-        featuresFromRow = FeaturesRowDecoder.apply(inputSchema, mergedGFs),
+      val cfg = ApplyModelsDistributedConfig(
+        featuresFromRow = FeaturesRowDecoder.apply(inputSchema, mergedGFs), // row decoders
         models_featuresIdx = modelObjects.zip(featuresIndices),
         keepColumnsIndices = keepColsNames.map(inputSchema.fieldIndex)
       )
 
-      spark.sparkContext.broadcast(tdc)
+      spark.sparkContext.broadcast(cfg)
     }
 
     // each row produce N rows, where N = models.size
@@ -123,9 +129,9 @@ class ApplyModelsTransformer(override val uid: String) extends Transformer with 
    */
   protected def buildConfig(): ApplyModelsTransformerConfig = {
     val keepColumns: Seq[String] = DescriptionTools.extractKeepColumns(getKeepColumns)
-    logInfo(s"Columns to keep: <${getKeepColumns}>, decoded as <${keepColumns.mkString("'", "', '", "'")}>")
+    logInfo(s"Columns to keep: <${getKeepColumns}>, decoded as [${keepColumns.mkString("'", "', '", "'")}]")
 
-    val modelsObjects: Seq[ComplexMLModel] = {
+    val modelsList: Seq[ComplexMLModel] = {
       logInfo(s"Decoding models description: <${getModels}> ...")
       val descriptions: Seq[ModelDescription] = DescriptionTools.extractModelsDescriptions(getModels)
       logInfo(s"Parsed ${descriptions.size} models descriptions")
@@ -138,9 +144,9 @@ class ApplyModelsTransformer(override val uid: String) extends Transformer with 
         model
       })
     }
-    logInfo(s"Loaded ${modelsObjects.count(m => m.isOK)} valid models")
+    logInfo(s"Loaded ${modelsList.count(m => m.isOK)} valid models")
 
-    ApplyModelsTransformerConfig(modelsObjects, keepColumns)
+    ApplyModelsTransformerConfig(modelsList, keepColumns)
   }
 
   /**
@@ -148,13 +154,13 @@ class ApplyModelsTransformer(override val uid: String) extends Transformer with 
    * Keeping this method here provides the ability to test transformer with mock models.
    *
    * @param description model metadata, should contain known `model_type` parameter
-   * @return model with `apply` implementation
+   * @return model object
    */
   protected def loadModel(description: ModelDescription): ComplexMLModel = {
     // InvalidModel chosen against Option[Model] for `isOK` attribute sake. Probably wrong solution.
 
     val modelType: Option[String] = DescriptionTools.getModelType(description)
-    logInfo(s"model type: <${modelType}>")
+    logInfo(s"model type: '${modelType}'")
 
     def unbox(loaded: Try[ComplexMLModel]): ComplexMLModel = loaded match {
       case Success(m) => m
@@ -163,14 +169,14 @@ class ApplyModelsTransformer(override val uid: String) extends Transformer with 
         InvalidModel(description, err.getMessage)
     }
 
-    // TODO: should be factored out as registry/factory, you can drop BaseLineDummy then
+    // TODO: should be factored out as registry/factory; you can drop BaseLineDummy then
     modelType match {
       case Some("ScoringCustomLogRegSA") => unbox(loadScoreAudienceModel(description))
       case Some("LalTfidfScaledSgdcPLSA") => unbox(loadLalTfidfScaledSgdcModel(description))
       case Some("LalBinarizedMultinomialNbPLSA") => unbox(loadLalBinarizedMultinomialNbModel(description))
       case Some("BaseLineDummy") => BaseLineModel(description) // n.b. not a best way to facilitate it tests
       case _ =>
-        logError(s"model loading failed, unknown model type `${modelType}`; description `${description}`")
+        logError(s"model loading failed, unknown model type '${modelType}'; description '${description}'")
         InvalidModel(description, "unknown model type")
     }
   }
@@ -179,30 +185,7 @@ class ApplyModelsTransformer(override val uid: String) extends Transformer with 
 
 object ApplyModelsTransformer extends DefaultParamsReadable[ApplyModelsTransformer] {
 
-  def apply(modelsList: String, keepColumnsList: String): ApplyModelsTransformer = {
-    val trf = new ApplyModelsTransformer()
-
-    trf
-      .setModels(modelsList)
-      .setKeepColumns(keepColumnsList)
-  }
-
-  private def applyModels(df: DataFrame, outSchema: StructType, broadcasted: Broadcast[ApplyModelsDistributedConfig]): DataFrame =
-    df.mapPartitions(applyModelsToIter(_, broadcasted))(RowEncoder(outSchema))
-
-  private def applyModelsToIter(rows: Iterator[Row], broadcasted: Broadcast[ApplyModelsDistributedConfig]): Iterator[Row] =
-    rows.flatMap(applyModelsToRow(_, broadcasted.value))
-
-  private def applyModelsToRow(row: Row, cfg: ApplyModelsDistributedConfig): Seq[Row] = {
-    val keepCols = cfg.keepColumnsIndices.map(idx => row(idx))
-    val allFeatures = cfg.featuresFromRow.decode(row)
-
-    cfg.models_featuresIdx.map { case (model, featuresIdx) =>
-      Row.fromSeq(keepCols ++
-        model.apply(GroupedFeatures.sliceFeaturesVector(allFeatures, featuresIdx)))
-    }
-  }
-
+  // TODO: consider replacing this with combination of interfaces and adapters + 'required schema'
   private val predefinedScoringFields = Seq(
     StructField("score", DataTypes.DoubleType),
     StructField("scores_raw", DataTypes.createArrayType(DataTypes.DoubleType)),
@@ -211,18 +194,68 @@ object ApplyModelsTransformer extends DefaultParamsReadable[ApplyModelsTransform
     StructField("category", DataTypes.StringType)
   )
 
+  /**
+   * Factory
+   * @param modelsList models description list, encoded
+   * @param keepColumnsList columns names to keep during transformation, encoded
+   * @return transformer object
+   */
+  def apply(modelsList: String, keepColumnsList: String): ApplyModelsTransformer = {
+    val trf = new ApplyModelsTransformer()
+
+    trf
+      .setModels(modelsList)
+      .setKeepColumns(keepColumnsList)
+  }
+
+  def applyModels(df: DataFrame, outSchema: StructType, broadcasted: Broadcast[ApplyModelsDistributedConfig]): DataFrame =
+    df.mapPartitions(
+      applyModelsToIter(_, broadcasted)
+    )(RowEncoder(outSchema))
+
+  private def applyModelsToIter(rows: Iterator[Row], broadcasted: Broadcast[ApplyModelsDistributedConfig]): Iterator[Row] =
+    rows.flatMap(
+      applyModelsToRow(_, broadcasted.value)
+    )
+
+  private def applyModelsToRow(row: Row, cfg: ApplyModelsDistributedConfig): Seq[Row] = {
+    import GroupedFeatures.{sliceFeaturesVector => slice}
+    val keepValues = cfg.keepColumnsIndices.map(idx => row(idx))
+    val allFeaturesArray = cfg.featuresFromRow.decode(row)
+
+    // one row => models.size rows
+    cfg.models_featuresIdx.map { case (model, featuresIndices) =>
+      Row.fromSeq(
+        keepValues ++ model.apply(slice(allFeaturesArray, featuresIndices))
+      )
+    }
+  }
+
   type ModelDescription = Map[String, String]
 
   object DescriptionTools {
     import StringToolbox._
 
-    // separators for text in `models` parameter
-    val CFG_MODELS_SEPARATOR = ";"
-    val CFG_MODEL_PARAMETERS_SEPARATOR = ","
-    val CFG_MODEL_KV_SEPARATOR = "->"
-    val CFG_COLUMNS_SEPARATOR = ","
+    // TODO: consider replacing this dumb solution with old plain json codec
 
-    def extractModelDescription(text: String): ModelDescription = {
+    // separators for text in `models` parameter
+    val CFG_MODELS_SEPARATOR = ";" // list of models: model_descr1;model_descr2;...
+    val CFG_MODEL_PARAMETERS_SEPARATOR = "," // list of pairs: k1->v1,k2->v2,...
+    val CFG_MODEL_KV_SEPARATOR = "->" // pair: a->b
+    val CFG_COLUMNS_SEPARATOR = "," // list of names: col1,col2,...
+
+    val CFG_MODEL_TYPE_KEY = "model_type" // fixed model type name is stored under that key
+
+    def getModelType(descr: ModelDescription): Option[String] = descr.get(CFG_MODEL_TYPE_KEY)
+
+    // decode transformer parameter to list of models descriptions
+    def extractModelsDescriptions(text: String): Seq[ModelDescription] = {
+      implicit val sep: Separators = Separators(CFG_MODELS_SEPARATOR)
+
+      text.splitTrim.map(extractModelDescription)
+    }
+
+    private def extractModelDescription(text: String): ModelDescription = {
       implicit val sep: Separators = Separators(
         CFG_MODEL_PARAMETERS_SEPARATOR,
         Some(Separators(CFG_MODEL_KV_SEPARATOR))
@@ -231,37 +264,32 @@ object ApplyModelsTransformer extends DefaultParamsReadable[ApplyModelsTransform
       text.parseMap
     }
 
-    def extractModelsDescriptions(text: String): Seq[ModelDescription] = {
-      implicit val sep: Separators = Separators(CFG_MODELS_SEPARATOR)
-
-      text.splitTrim.map(extractModelDescription)
-    }
-
-    def getModelType(descr: ModelDescription): Option[String] = descr.get("model_type")
-
-    def packModelDescription(descr: ModelDescription): String = {
-      descr.toList.map { case (k, v) => s"${k}${DescriptionTools.CFG_MODEL_KV_SEPARATOR}${v}" }
-        .sorted
-        .mkString(DescriptionTools.CFG_MODEL_PARAMETERS_SEPARATOR)
-    }
-
-    def packModelsDescriptions(descrs: Seq[ModelDescription]): String = {
-      descrs.map(packModelDescription).mkString(CFG_MODELS_SEPARATOR)
-    }
-
     def extractKeepColumns(text: String): Seq[String] = {
       implicit val sep: Separators = Separators(CFG_COLUMNS_SEPARATOR)
 
       text.splitTrim
     }
 
-    def packKeepColumns(cols: Seq[String]): String = cols.mkString(CFG_COLUMNS_SEPARATOR)
+    // pack <-> extract
+
+    def packModelsDescriptions(descrs: Seq[ModelDescription]): String =
+      descrs.map(packModelDescription).mkString(CFG_MODELS_SEPARATOR)
+
+    private def packModelDescription(descr: ModelDescription): String = {
+      descr.toList.map { case (k, v) => s"${k}${DescriptionTools.CFG_MODEL_KV_SEPARATOR}${v}" }
+        .sorted
+        .mkString(DescriptionTools.CFG_MODEL_PARAMETERS_SEPARATOR)
+    }
+
+    def packKeepColumns(cols: Seq[String]): String =
+      cols.mkString(CFG_COLUMNS_SEPARATOR)
 
   }
 
   def loadScoreAudienceModel(description: ModelDescription): Try[ScoreAudienceModel] = Try {
       val audienceName = description("audience_name")
       val json = loadJsonFromFile(description("model_path"))
+
       val predictor = ModelsConfigJson.loadScoreAudiencePredictorFromJson(json)
 
       ScoreAudienceModel(audienceName, predictor)
@@ -300,33 +328,41 @@ object ApplyModelsTransformer extends DefaultParamsReadable[ApplyModelsTransform
   private def loadJsonFromFile(path: String) = JsonToolbox.parseJson(readTextFile(path))
 
   /**
-    * Try to load file from SparkFiles local path, if no good, load file from local path, finally try HDFS
+    * Try to load file from SparkFiles local path; if no good, load file from local path; finally try HDFS
     *
-    * @return file body
+    * @return file body or exception
     */
   private def readTextFile(path: String): String = {
     import org.slf4j.LoggerFactory
     val log = LoggerFactory.getLogger(getClass)
-    log.info(s"loading file <${path}> ...")
+    log.info(s"loading file '${path}' ...")
 
     val trySparkFiles = Try {
-      val fileDirName = FileToolbox.getPathBasename(FileToolbox.getPathDirname(path))
-      val sparkFilesPath = FileToolbox.joinPath(SparkFiles.get(fileDirName), FileToolbox.getPathBasename(path))
+      val sparkFilesPath = {
+        val lastDirName = FileToolbox.getPathBasename(
+          FileToolbox.getPathDirname(path)
+        )
 
-      //log.info(s"loading local text file <${sparkFilesPath}> ...")
+        FileToolbox.joinPath(
+          SparkFiles.get(lastDirName),
+          FileToolbox.getPathBasename(path)
+        )
+      }
+
+      log.debug(s"loading local text file '${sparkFilesPath}' ...")
       FileToolbox.loadTextFile(sparkFilesPath)
     }
 
     val tryLocalPath = trySparkFiles.recoverWith { case err =>
       log.warn(s"loading text file from SparkFiles cache failed, exception: ${err}")
       Try {
-        //log.info(s"loading local text file <${path}> ...")
+        log.debug(s"loading local text file '${path}' ...")
         FileToolbox.loadTextFile(path)
-      }}
+    }}
 
     val tryHdfs = tryLocalPath.recover { case err =>
       log.warn(s"loading local text file failed, exception: ${err}")
-      //log.info(s"loading text file from HDFS <${path}> ...")
+      log.debug(s"loading text file from HDFS '${path}' ...")
       HDFSFileToolbox.loadTextFile(path)
     }
 
