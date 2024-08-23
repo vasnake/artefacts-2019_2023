fbe859adc4a89ebeed5f084ac60c38fcb4ee6fd3
Valentin Fedulov <vasnake@gmail.com>
2024-08-23 18:45:25 +0300

    added build script

---
+++ cicd/build_uber_jar.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -ex
+
+SBT_PROJECT_DIR=$1 # ../etl-ml-pieces.scala
+PACKAGES_DIR=$2 # /tmp/workdir/PACKAGES
+
+# debug
+echo SBT_PROJECT_DIR ${SBT_PROJECT_DIR}
+echo PACKAGES_DIR ${PACKAGES_DIR}
+pwd
+ls -la
+whoami
+env
+java -version
+javac -version
+
+if [ ! -f "${SBT_PROJECT_DIR}/build.sbt" ]; then
+    echo "Unknown sbt project, check the first argument"
+    exit 1
+fi
+
+if [ -z "${PACKAGES_DIR}" ]; then
+    echo "Target dir must be given, check the second argument"
+fi
+
+export JAVA_OPTS="${JAVA_OPTS} -Dsbt.log.noformat=true"
+
+pushd "${SBT_PROJECT_DIR}"
+timeout -v -k 5s 30m sbt -v --mem 4096 clean test assembly
+#    -k duration
+#   --kill-after=duration
+popd
+
+mkdir -p ${PACKAGES_DIR}
+cp "${SBT_PROJECT_DIR}/target/scala-2.12/*.jar" ${PACKAGES_DIR}
+
+# debug
+ls -lh ${PACKAGES_DIR}
